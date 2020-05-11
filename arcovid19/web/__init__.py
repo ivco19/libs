ff7fcf52d33d54c2b900d497c38de9f68badde8d
Juan B Cabral <jbc.develop@gmail.com>
2020-05-11 17:10:01 -0300

    giupone a todos lados

---
+++ arcovid19/web/__init__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2020, Bruno Sanchez, Vanessa Daza,
+#                     Juan B Cabral, Marcelo Lares,
+#                     Nadia Luczywo, Dante Paz, Rodrigo Quiroga,
+#                     Martín de los Ríos, Federico Stasyszyn
+#                     Cristian Giuppone.
+# License: BSD-3-Clause
+#   Full Text: https://raw.githubusercontent.com/ivco19/libs/master/LICENSE
+
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""This package contains the entire flask app logic to run a web server.
+
+This file contain the get_app method, but you can access directly the
+implementation of all the project with the blueprint
+``arcovid19.web.bp.wavid19``.
+
+"""
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import flask
+
+from . import bp
+
+
+# =============================================================================
+# PUBLIC API
+# =============================================================================
+
+def get_webapp(**kwargs):
+    app = flask.Flask("arcovid19.web")
+    app.register_blueprint(bp.wavid19)
+    return app
---
+++ arcovid19/web/bp.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2020, Bruno Sanchez, Mauricio Koraj, Vanessa Daza,
+#                     Juan B Cabral, Mariano Dominguez, Marcelo Lares,
+#                     Nadia Luczywo, Dante Paz, Rodrigo Quiroga,
+#                     Martín de los Ríos, Federico Stasyszyn,
+#                     Cristian Giuppone.
+# License: BSD-3-Clause
+#   Full Text: https://raw.githubusercontent.com/ivco19/libs/master/LICENSE
+
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""Core functionalities for arcovid19
+
+"""
+
+__all__ = ["wavid19"]
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import flask
+
+
+# =============================================================================
+# Blueprint
+# =============================================================================
