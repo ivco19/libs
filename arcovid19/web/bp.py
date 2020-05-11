5816dd5ff0016cd9cd3ba65098a63a0c026ee68e
Juan B Cabral <jbc.develop@gmail.com>
2020-05-11 17:38:30 -0300

    templates orchectration done

--- arcovid19/web/bp.py
+++ arcovid19/web/bp.py
@@ -26,8 +26,47 @@ __all__ = ["wavid19"]
 # =============================================================================
 
 import flask
+from flask.views import View
+
+# =============================================================================
+# BASE CLASS
+# =============================================================================
+
+class TemplateView(View):
+
+    def get_template_name(self):
+        return self.template_name
+
+    def get_context_data(self):
+        return {}
+
+    def render_template(self, context):
+        return flask.render_template(self.get_template_name(), **context)
+
+    def dispatch_request(self):
+        context = self.get_context_data()
+        return self.render_template(context)
+
+
+# =============================================================================
+# VIEWS
+# =============================================================================
+
+class InfectionCurveView(TemplateView):
+    template_name = "InfectionCurve.html"
 
 
 # =============================================================================
 # Blueprint
 # =============================================================================
+
+wavid19 = flask.Blueprint("arcovid19", "arcovid19.web.bp")
+
+wavid19.add_url_rule(
+    '/', view_func=InfectionCurveView.as_view("index"))
+wavid19.add_url_rule(
+    '/icurve', view_func=InfectionCurveView.as_view("ivcurve"))
+
+
+
+
